7e7fbd7d0b6226ce2c724266f5cf3661a0967f0d
richglezriv <richglezriv@hotmail.com>
2015-05-11 23:35:26 -0500

    set wa

---
+++ backend-net/ReaccionDigital.LMS/RD.LMS/Controllers/CoursesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RD.LMS.Controllers
+{
+    public class CoursesController : Controller
+    {
+        //
+        // GET: /Courses/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Get()
+        {
+            string test = "{scorm_model: {status: success, data: {actionRequested:, params:}";
+
+            return Json(test);
+        }
+    }
+}
---
+++ backend-net/ReaccionDigital.LMS/RD.LMS/Models/CourseModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RD.LMS.Models
+{
+    public class CourseModel : IDataModel
+    {
+        public String id { get; set; }
+
+        public String name { get; set; }
+
+        public String scoPath { get; set; }
+
+        public String scoIndex { get; set; }
+
+        public String status { get; set; }
+
+        //public LMS scorm_data { get; set; }
+        public String dataModel { get; set; }
+        //public System.Web.Mvc.JsonResult scorm_data { get; set; }
+
+        
+    }
+}
