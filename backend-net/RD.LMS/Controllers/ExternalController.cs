19f66965be4236548ab86bf61b43dcb93318d959
richglezriv <richglezriv@hotmail.com>
2015-07-14 19:12:56 -0500

    external login key validation

--- backend-net/RD.LMS/Controllers/ExternalController.cs
+++ backend-net/RD.LMS/Controllers/ExternalController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -19,6 +20,25 @@ namespace RD.LMS.Controllers
         public ActionResult LoginUser(string data)
         {
             Models.JSonModel model = new Models.JSonModel();
+
+            try
+            {
+                const String KEY = "ZaZaCruREcHA8a7*as";
+                Newtonsoft.Json.Linq.JObject json = (Newtonsoft.Json.Linq.JObject)Newtonsoft.Json.JsonConvert.DeserializeObject(Request.QueryString[0].ToString());
+                string userId = json["data"]["userId"].ToString();
+                string serialKey = String.Format("{0}{1}", userId, KEY);
+                System.Security.Cryptography.SHA512 hashKey = System.Security.Cryptography.SHA512.Create();
+                hashKey.ComputeHash(Encoding.UTF8.GetBytes(serialKey));
+                string hashString = BitConverter.ToString(hashKey.Hash).Replace("-", String.Empty).ToLower();
+
+                if (!json["data"]["key"].ToString().Equals(hashString))
+                    model.status = "fail";
+            }
+            catch (Exception)
+            {
+                model.status = "fail";
+            }
+
 
             return Json(model, JsonRequestBehavior.AllowGet);
         }
